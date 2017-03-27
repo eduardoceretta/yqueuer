5e45d023e69b38b8fd31823876b0054457093bba
Eduardo C Dalla Favera <eduardo.ceretta+github@gmail.com>
2017-03-28 00:16:00 +0200

    Adding dj-database-url helper to load heroku db

--- yqueuer/settings.py
+++ yqueuer/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import sys
 import re
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,6 +120,11 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+print db_from_env
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
